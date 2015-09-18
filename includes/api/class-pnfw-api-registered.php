0a9baea985443f6c63a5f3a3221194fd414f5c89
delitestudio <delitestudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-18 08:30:38 +0000

    Removed activation control, moved to another class.
    git-svn-id: https://plugins.svn.wordpress.org/push-notifications-for-wp/trunk@1248252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/api/class-pnfw-api-registered.php
+++ includes/api/class-pnfw-api-registered.php
@@ -11,18 +11,9 @@ class PNFW_API_Registered extends PNFW_API {
   if ($this->is_token_missing())
    $this->json_error('401', __("Token not registered.\nTo solve the problem please uninstall and reinstall the app.", 'pnfw'));
 
-  if (!$this->is_token_activated())
-   $this->json_error('401', __('Your email needs to be verified. Go to your email inbox and find the message from us asking you to confirm your address. Or make sure your email address is entered correctly.', 'pnfw'));
-
   // Update lang
   if (isset($this->lang))
    $this->update_token_lang();
- }
-
- private function is_token_activated() {
-  global $wpdb;
-  $push_tokens = $wpdb->get_blog_prefix().'push_tokens';
-  return (boolean)$wpdb->get_var($wpdb->prepare("SELECT active FROM $push_tokens WHERE token=%s AND os=%s", $this->token, $this->os));
  }
 
  private function update_token_lang() {
@@ -39,4 +30,5 @@ class PNFW_API_Registered extends PNFW_API {
    )
   );
  }
+
 }
